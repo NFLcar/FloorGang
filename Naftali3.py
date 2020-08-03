a712ded97cfa26b9c9a6dcc3be6ab8ed9b668db2
NFLcar <69142584+NFLcar@users.noreply.github.com>
2020-08-03 14:30:20 +0300

    Add files via upload

---
+++ Naftali3.py
@@ -0,0 +1,49 @@
+def do_turn(pw):
+	if len(pw.my_planets()) == 0:
+		return
+
+	if len(pw.enemy_planets()) == 0:
+		return
+
+	source = pw.get_planet(findBiggestPlanet(pw.my_planets()))
+
+	if len(pw.neutral_planets()) >= 1:
+		dest = findClosestPlanetToDest(source, pw.neutral_planets(), pw)
+	else:
+		if len(pw.enemy_planets()) >= 1:
+			dest = findClosestPlanetToDest(source, pw.enemy_planets(), pw)
+	
+	num_ships = source.num_ships() / 2
+	pw.debug('Num Ships: ' + str(num_ships))
+
+	pw.issue_order(source, dest, num_ships)
+
+def findSmallestPlanet(planets):
+	smallestPlanetId = 0
+	smallestPlanetSize = 999999999999
+	for planet in planets:
+		if planet.num_ships() < smallestPlanetSize:
+			smallestPlanetId = planet.planet_id()
+			smallestPlanetSize = planet.num_ships()
+
+	return smallestPlanetId
+
+def findBiggestPlanet(planets):
+	biggestPlanetId = 0
+	biggestPlanetSize = 0
+	for planet in planets:
+		if planet.num_ships() > biggestPlanetSize:
+			biggestPlanetId = planet.planet_id()
+			biggestPlanetSize = planet.num_ships()
+
+	return biggestPlanetId
+
+def findClosestPlanetToDest(dest, planets, pw):
+	closestPlanetDistance = 999999999999
+	closestPlanet = dest
+	for planet in planets:
+		if closestPlanetDistance > pw.distance(planet, dest):
+			closestPlanetDistance = pw.distance(planet, dest)
+			closestPlanet = planet
+	
+	return closestPlanet
