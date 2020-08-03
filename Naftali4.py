006e115aa7ca7f6478586e459736839340ee6303
NFLcar <69142584+NFLcar@users.noreply.github.com>
2020-08-03 15:05:31 +0300

    Add files via upload

---
+++ Naftali4.py
@@ -0,0 +1,48 @@
+def do_turn(pw):
+	if len(pw.my_planets()) == 0:
+		return
+
+	if len(pw.enemy_planets()) == 0:
+		return
+
+	for planet in pw.my_planets():
+
+		source = planet
+
+		if len(pw.not_my_planets()) >= 1:
+			dest = findClosestPlanetToDest(source, pw.not_my_planets(), pw)
+	
+		num_ships = source.num_ships() / 10
+
+		if source.num_ships() > 30:
+			pw.issue_order(source, dest, num_ships)
+
+def findSmallestPlanet(planets):
+	smallestPlanetId = 0
+	smallestPlanetSize = 999999999999
+	for planet in planets:
+		if planet.num_ships() < smallestPlanetSize:
+			smallestPlanetId = planet.planet_id()
+			smallestPlanetSize = planet.num_ships()
+
+	return smallestPlanetId
+
+def findBiggestPlanet(planets):
+	biggestPlanetId = 0
+	biggestPlanetSize = 0
+	for planet in planets:
+		if planet.num_ships() > biggestPlanetSize:
+			biggestPlanetId = planet.planet_id()
+			biggestPlanetSize = planet.num_ships()
+
+	return biggestPlanetId
+
+def findClosestPlanetToDest(dest, planets, pw):
+	closestPlanetDistance = 999999999999
+	closestPlanet = dest
+	for planet in planets:
+		if closestPlanetDistance > pw.distance(planet, dest):
+			closestPlanetDistance = pw.distance(planet, dest)
+			closestPlanet = planet
+	
+	return closestPlanet
